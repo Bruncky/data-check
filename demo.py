b3efefb6c2e5a7dcecf4ca36b4796e387e06706e
Bruno C. Brunckhorst <brunobrunckhorst@gmail.com>
2022-10-10 15:50:05 +0200

    Completed challenge 1

---
+++ demo.py
@@ -0,0 +1,11 @@
+"""Dummy challenge for Kitt Demo"""
+
+
+
+def circle_area(radius):
+    """Returns the area of the circle of given radius"""
+    # Guard clause
+    if radius < 0:
+        return 0
+
+    return 3.14 * radius **2
